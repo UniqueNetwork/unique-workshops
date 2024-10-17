853d5de40151b9f7a6681873e0c50b46bc7d0fad
Max Andreev <maxick20@gmail.com>
2024-10-17 15:59:25 +0400

    add more logs

--- src/main.ts
+++ src/main.ts
@@ -3,9 +3,13 @@ import { createNFTs } from "./nft";
 
 const main = async () => {
   const collection = await createCollection();
+  console.log("Creating a collection. Please wait...");
   console.log(
     `Collection created: https://uniquescan.io/opal/collections/${collection}`
   );
+
+  
+  console.log("Creating NFTs. Please wait...");
   const nfts = await createNFTs(collection);
   console.log("NFTs created");
 };
